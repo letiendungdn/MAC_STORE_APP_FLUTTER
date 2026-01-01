a323206ac95567fbf31fe913490b34caf5b5e976
Dung T. Le <dungle@hayworkglobal.com>
2026-01-01 13:58:53 +0700

    37 - Product Detail Screen

--- lib/views/screens/nav_screens/widgets/product_item_widget.dart
+++ lib/views/screens/nav_screens/widgets/product_item_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mac_store_app/models/product.dart';
+import 'package:mac_store_app/views/screens/detail/screens/product_detail_screen.dart';
 
 class ProductItemWidget extends StatelessWidget {
   final Product product;
@@ -9,67 +10,80 @@ class ProductItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 170,
-      margin: const EdgeInsets.symmetric(horizontal: 8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 170,
-            decoration: BoxDecoration(
-              color: const Color(0xFFF2F2F2),
-              borderRadius: BorderRadius.circular(24),
-            ),
-            child: Stack(
-              children: [
-                Image.network(
-                  product.images[0],
-                  height: 170,
-                  width: 170,
-                  fit: BoxFit.cover,
-                ),
-                Positioned(
-                  top: 15,
-                  right: 2,
-                  child: Image.asset(
-                    'assets/icons/love.png',
-                    width: 26,
-                    height: 26,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProductDetailScreen(product: product),
+          ),
+        );
+      },
+      child: Container(
+        width: 170,
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 170,
+              decoration: BoxDecoration(
+                color: const Color(0xFFF2F2F2),
+                borderRadius: BorderRadius.circular(24),
+              ),
+              child: Stack(
+                children: [
+                  Image.network(
+                    product.images.isNotEmpty ? product.images[0] : '',
+                    height: 170,
+                    width: 170,
+                    fit: BoxFit.cover,
+                    errorBuilder: (_, __, ___) => const Center(
+                      child: Icon(Icons.broken_image_outlined),
+                    ),
                   ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: Image.asset(
-                    'assets/icons/cart.png',
-                    height: 26,
-                    width: 26,
+                  Positioned(
+                    top: 15,
+                    right: 2,
+                    child: Image.asset(
+                      'assets/icons/love.png',
+                      width: 26,
+                      height: 26,
+                    ),
                   ),
-                ),
-              ],
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Image.asset(
+                      'assets/icons/cart.png',
+                      height: 26,
+                      width: 26,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            product.productName,
-            overflow: TextOverflow.ellipsis,
-            style: GoogleFonts.roboto(
-              fontSize: 14,
-              color: const Color(0xFF212121),
-              fontWeight: FontWeight.bold,
+            const SizedBox(height: 8),
+            Text(
+              product.productName,
+              overflow: TextOverflow.ellipsis,
+              style: GoogleFonts.roboto(
+                fontSize: 14,
+                color: const Color(0xFF212121),
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            product.category,
-            style: GoogleFonts.quicksand(
-              fontSize: 13,
-              fontWeight: FontWeight.bold,
-              color: const Color(0xff868D94),
+            const SizedBox(height: 4),
+            Text(
+              product.category,
+              style: GoogleFonts.quicksand(
+                fontSize: 13,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xff868D94),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
